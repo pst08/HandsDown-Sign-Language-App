7260bf6948a0e2b7cd9806c3c47328cc18e803e8
pst08 <preethyt04@gmail.com>
2025-07-12 12:22:55 +0530

    Initialising frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>👋 HandsDown – Sign Language App</h1>
+      <p>React frontend is working!</p>
+    </div>
+  );
+}
+
+export default App;
